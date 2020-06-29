5aef2aeea3e3953476c1cca368aa48f799c96e84
Kevin <56485821+kevofwar227@users.noreply.github.com>
2020-06-29 15:56:43 -0700

    Add files via upload

---
+++ consoleCalculator/consoleCalculator/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace consoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //writing the operator options.
+            static void operators()
+            {
+                Console.WriteLine("Please choose an operator: +, -, *, /. Enter exit to close. All values are case sensitive!");
+                string op = Console.ReadLine();
+
+                //writing the addition operator of the calculator
+                if (op == "+")
+                {
+                    Console.WriteLine("Please enter a number: ");
+                    int add1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter another number: ");
+                    int add2 = Convert.ToInt32(Console.ReadLine());
+
+                    int add = Convert.ToInt32(add1 + add2);
+                    Console.WriteLine("The result is {0}", add);
+
+                    operators();
+                }
+                //writing the subtraction operator
+                if (op == "-")
+                {
+                    Console.WriteLine("Please enter a number: ");
+                    int sub1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter another number: ");
+                    int sub2 = Convert.ToInt32(Console.ReadLine());
+
+                    int sub = Convert.ToInt32(sub1 - sub2);
+                    Console.WriteLine("The result is {0}", sub);
+                    operators();
+                }
+                //multiplication operator
+                if (op == "*")
+                {
+                    Console.WriteLine("Please enter a number: ");
+                    int mult1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter another number: ");
+                    int mult2 = Convert.ToInt32(Console.ReadLine());
+
+                    int mult = Convert.ToInt32(mult1 * mult2);
+                    Console.WriteLine("The result is {0}", mult);
+                    operators();
+                }
+                //division operator
+                if (op == "/")
+                {
+                    Console.WriteLine("Please enter a number: ");
+                    int div1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter another number: ");
+                    int div2 = Convert.ToInt32(Console.ReadLine());
+
+                    int div = Convert.ToInt32(div1 / div2);
+                    Console.WriteLine("The result is {0}", div);
+                    operators();
+                }
+                //exit protocall
+                if (op == "exit")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Ended...Thanks for using consoleCalculator V.1");
+                }
+            }
+            operators();
+        }
+    }
+}
